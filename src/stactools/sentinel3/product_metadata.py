850c1d2fe0797252cf68dcb4d8f92bef62f83ab0
chorng <chung.horng@sinergise.com>
2021-09-14 19:09:59 +0200

    Add SYNERGY and SRAL

--- src/stactools/sentinel3/product_metadata.py
+++ src/stactools/sentinel3/product_metadata.py
@@ -141,7 +141,7 @@ class ProductMetadata:
     def metadata_dict(self) -> Dict[str, Any]:
         product_type = self._root.findall(".//sentinel3:productType")[0].text
         if (product_type.split("_")[0] == "OL"
-                and product_type.split("_")[1] == "1"):
+            and product_type.split("_")[1] == "1"):
             result = {
                 "start_datetime":
                 str(self.start_datetime),
@@ -423,7 +423,196 @@ class ProductMetadata:
                         self._root.find_attr("percentage",
                                              ".//slstr:outOfRangePixels"))),
             }
-
+        elif (product_type.split("_")[0] == "SR" 
+              and product_type.split("_")[1] == "2"):
+            result = {
+                "start_datetime":
+                str(self.start_datetime),
+                "end_datetime":
+                str(self.end_datetime),
+                "s3:instrument":
+                str(
+                    self._root.find_attr(
+                        "abbreviation",
+                        ".//sentinel-safe:familyName[@abbreviation]")),
+                "s3:mode":
+                str(self._root.find_attr("identifier",
+                                         ".//sentinel-safe:mode")),
+                "s3:productType":
+                self._root.findall(".//sentinel3:productType")[0].text,
+                "s3:lrmModePercentage":
+                float(self._root.findall(".//sral:lrmModePercentage")[0].text),
+                "s3:sarModePercentage":
+                float(self._root.findall(".//sral:sarModePercentage")[0].text),
+                "s3:landPercentage":
+                float(self._root.findall(".//sral:landPercentage")[0].text),
+                "s3:closedSeaPercentage":
+                float(self._root.findall(".//sral:closedSeaPercentage")[0].text),
+                "s3:continentalIcePercentage":
+                float(self._root.findall(".//sral:continentalIcePercentage")[0].text),
+                "s3:openOceanPercentage":
+                float(self._root.findall(".//sral:openOceanPercentage")[0].text),
+            }
+        elif "SY_2_AOD" in product_type:
+            result = {
+                "start_datetime":
+                str(self.start_datetime),
+                "end_datetime":
+                str(self.end_datetime),
+                "s3:instrument":
+                str(
+                    self._root.find_attr(
+                        "abbreviation",
+                        ".//sentinel-safe:familyName[@abbreviation]")),
+                "s3:mode":
+                str(self._root.find_attr("identifier",
+                                         ".//sentinel-safe:mode")),
+                "s3:productType":
+                self._root.findall(".//sentinel3:productType")[0].text,
+                "s3:salineWaterPixels_percentage":
+                float(
+                    str(
+                        self._root.find_attr("percentage", 
+                                             ".//sentinel3:salineWaterPixels"))),
+                "s3:landPixels_percentage":
+                float(str(self._root.find_attr("percentage",
+                                               ".//sentinel3:landPixels"))),
+            }
+        elif "SY_2_SYN" in product_type:
+            result = {
+                "start_datetime":
+                str(self.start_datetime),
+                "end_datetime":
+                str(self.end_datetime),
+                "s3:instrument":
+                str(
+                    self._root.find_attr(
+                        "abbreviation",
+                        ".//sentinel-safe:familyName[@abbreviation]")),
+                "s3:mode":
+                str(self._root.find_attr("identifier",
+                                         ".//sentinel-safe:mode")),
+                "s3:productType":
+                self._root.findall(".//sentinel3:productType")[0].text,
+                "s3:salineWaterPixels_percentage":
+                float(
+                    str(
+                        self._root.find_attr("percentage", 
+                                             ".//sentinel3:salineWaterPixels"))),
+                "s3:coastalPixels_percentage":
+                float(
+                    str(
+                        self._root.find_attr("percentage", 
+                                             ".//sentinel3:coastalPixels"))),
+                "s3:freshInlandWaterPixels_percentage":
+                float(
+                    str(
+                        self._root.find_attr("percentage", 
+                                             ".//sentinel3:freshInlandWaterPixels"))),
+                "s3:tidalRegionPixels_percentage":
+                float(
+                    str(
+                        self._root.find_attr("percentage", 
+                                             ".//sentinel3:tidalRegionPixels"))),
+                "s3:landPixels_percentage":
+                float(str(self._root.find_attr("percentage",
+                                               ".//sentinel3:landPixels"))),
+            }
+        elif "SY_2_V10" in product_type:
+            result = {
+                "start_datetime":
+                str(self.start_datetime),
+                "end_datetime":
+                str(self.end_datetime),
+                "s3:instrument":
+                str(
+                    self._root.find_attr(
+                        "abbreviation",
+                        ".//sentinel-safe:familyName[@abbreviation]")),
+                "s3:mode":
+                str(self._root.find_attr("identifier",
+                                         ".//sentinel-safe:mode")),
+                "s3:productType":
+                self._root.findall(".//sentinel3:productType")[0].text,
+                "s3:snowOrIcePixels_percentage":
+                float(
+                    str(
+                        self._root.find_attr("percentage", 
+                                             ".//sentinel3:snowOrIcePixels"))),
+                "s3:landPixels_percentage":
+                float(str(self._root.find_attr("percentage",
+                                               ".//sentinel3:landPixels"))),
+            }
+        elif "SY_2_VG1" in product_type:
+            result = {
+                "start_datetime":
+                str(self.start_datetime),
+                "end_datetime":
+                str(self.end_datetime),
+                "s3:instrument":
+                str(
+                    self._root.find_attr(
+                        "abbreviation",
+                        ".//sentinel-safe:familyName[@abbreviation]")),
+                "s3:mode":
+                str(self._root.find_attr("identifier",
+                                         ".//sentinel-safe:mode")),
+                "s3:productType":
+                self._root.findall(".//sentinel3:productType")[0].text,
+                "s3:snowOrIcePixels_percentage":
+                float(
+                    str(
+                        self._root.find_attr("percentage", 
+                                             ".//sentinel3:snowOrIcePixels"))),
+                "s3:landPixels_percentage":
+                float(str(self._root.find_attr("percentage",
+                                               ".//sentinel3:landPixels"))),
+            }
+        elif "SY_2_VGP" in product_type:
+            result = {
+                "start_datetime":
+                str(self.start_datetime),
+                "end_datetime":
+                str(self.end_datetime),
+                "s3:instrument":
+                str(
+                    self._root.find_attr(
+                        "abbreviation",
+                        ".//sentinel-safe:familyName[@abbreviation]")),
+                "s3:mode":
+                str(self._root.find_attr("identifier",
+                                         ".//sentinel-safe:mode")),
+                "s3:productType":
+                self._root.findall(".//sentinel3:productType")[0].text,
+                "s3:snowOrIcePixels_percentage":
+                float(
+                    str(
+                        self._root.find_attr("percentage", 
+                                             ".//sentinel3:snowOrIcePixels"))),
+                "s3:salineWaterPixels_percentage":
+                float(
+                    str(
+                        self._root.find_attr("percentage", 
+                                             ".//sentinel3:salineWaterPixels"))),
+                "s3:coastalPixelss_percentage":
+                float(
+                    str(
+                        self._root.find_attr("percentage", 
+                                             ".//sentinel3:coastalPixels"))),
+                "s3:freshInlandWaterPixels_percentage":
+                float(
+                    str(
+                        self._root.find_attr("percentage", 
+                                             ".//sentinel3:freshInlandWaterPixels"))),
+                "s3:tidalRegionPixels_percentage":
+                float(
+                    str(
+                        self._root.find_attr("percentage", 
+                                             ".//sentinel3:tidalRegionPixels"))),
+                "s3:landPixels_percentage":
+                float(str(self._root.find_attr("percentage",
+                                               ".//sentinel3:landPixels"))),
+            }
         return {k: v for k, v in result.items() if v is not None}
 
     @property
